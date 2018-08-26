eefe1a72d1f5c02cc95fab9dff37afe045fbb221
valkoz <kozlov.valent@gmail.com>
2018-08-26 22:07:48 +0300

    Create DataProvider class to encapsulate data access logic , remove data access logic from presenter

--- app/src/main/java/com/github/valkoz/sigma/MainPresenter.java
+++ app/src/main/java/com/github/valkoz/sigma/MainPresenter.java
@@ -2,23 +2,10 @@ package com.github.valkoz.sigma;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.github.valkoz.sigma.model.TransformedItem;
-import com.github.valkoz.sigma.model.rss.RSS;
 
-import org.simpleframework.xml.core.Persister;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainPresenter implements MVPMainPresenter {
 
@@ -54,47 +41,16 @@ public class MainPresenter implements MVPMainPresenter {
     public static class LoadTask extends AsyncTask<String, Void, List<TransformedItem>> {
 
         private MVPMainView view;
-        private static final String PATH = "https://habr.com/rss/hubs/all/";
+        private DataProvider dataProvider;
 
         public LoadTask(MVPMainView mainView) {
             view = mainView;
+            dataProvider = new DataProvider();
         }
 
         @Override
         protected List<TransformedItem> doInBackground(String... uri) {
-
-            try {
-                URL url = new URL(PATH);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
-
-                    StringBuilder stringBuilder = new StringBuilder();
-                    try(BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            stringBuilder.append(line);
-                        }
-                    }
-
-                    Reader xmlReader = new StringReader(stringBuilder.toString());
-                    Persister serializer = new Persister();
-                    try {
-                        RSS rss = serializer.read(RSS.class, xmlReader, false);
-                        ItemTransformer transformer = new ItemTransformer();
-                        return transformer.transform(rss.getChannel().getItems());
-                    } catch (Exception e) {
-                        //RSS parse error
-                        Log.e(getClass().getCanonicalName(), e.getLocalizedMessage());
-                    }
-                } else {
-                    //Status != OK
-                    Log.e(getClass().getCanonicalName(), conn.getResponseMessage());
-                }
-            } catch (IOException e) {
-                //Connection error
-                Log.e(getClass().getCanonicalName(), e.getLocalizedMessage());
-            }
-            return null;
+            return dataProvider.getData();
         }
 
         @Override
